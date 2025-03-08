7259129a55f8631a440fa1a86da86fc3a86048d8
WitiaUA <108870328+WitiaUA@users.noreply.github.com>
2025-03-08 05:21:22 +0200

    Update script.js
    Фікс цифр

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 let maxValue = 100; // Скільки потрібно
 let currentValue = 30; // Скільки зібрано
 
+// Встановлення початкових значень у текст
+document.getElementById("collected").innerText = currentValue;
+document.getElementById("needed").innerText = maxValue;
+
 // Функція оновлення шкали
 function updateProgress() {
     let progress = (currentValue / maxValue) * 100;
     document.getElementById("progress").style.height = progress + "%";
-    
-    // Оновлення чисел
-    document.getElementById("collected").innerText = currentValue;
-    document.getElementById("needed").innerText = maxValue;
 }
 
 // Запускаємо оновлення
