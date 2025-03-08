76e580787cceaade0951f2d9db0f8c1d1b939225
WitiaUA <108870328+WitiaUA@users.noreply.github.com>
2025-03-08 06:35:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ function updateProgress() {
     let progress = (currentValue / maxValue) * 100;
     console.log("Оновлення шкали: " + progress + "%"); // Додати перевірку
     progressElement.style.height = progress + "%";
-    progressElement.style.backgroundColor = "green";
+    progressElement.style.backgroundColor = "#c8a400";
+
 }
 
 updateProgress();
